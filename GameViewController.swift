0fb7fb296e2436c44e89f153b7f60967e4153455
Jevier Izza Maulana <izzajevier@gmail.com>
2022-05-09 17:18:01 +0700

    Initial commit

---
+++ GameViewController.swift
@@ -0,0 +1,239 @@
+//
+//  GameViewController.swift
+//  Onely
+//
+//  Created by Jevier Izza Maulana on 28/04/22.
+//
+
+import UIKit
+
+import GameKit
+
+import AVFoundation
+
+class GameViewController: UIViewController {
+
+    @IBOutlet weak var numbersLabel: UILabel?
+    @IBOutlet weak var scoreLabel: UILabel?
+    @IBOutlet weak var inputField: UITextField?
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    public var score:Int = 0
+    
+    var timer:Timer?
+    var seconds:Int = 60
+    
+    var backgroundMusicPlayer: AVAudioPlayer!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        //playBackgrounMusic(filename: "Amusing_Day")
+        
+        setRandomNumberLabel()
+        updateScoreLabel()
+        
+        setTimer()
+        
+        inputField?.addTarget(self, action: #selector(GameViewController.textFieldDidChange(_:)), for: .editingChanged)
+    }
+    
+    func setTimer() {
+        if(timer == nil) {
+            timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(onUpdateTimer), userInfo: nil, repeats: true)
+            
+        }
+    }
+    
+//    func showHUDWithAnswer(isRight: Bool) {
+//        var imageView:UIImageView?
+//
+//        if isRight {
+//            ///
+//        }
+//        else {
+//
+//        }
+//
+//        if
+//    }
+    
+    @objc func textFieldDidChange(_ textField: UITextField) {
+        if inputField?.text?.count ?? 0 < 4 {
+            return
+        }
+        
+        if let numbers_text = numbersLabel?.text, let input_text = inputField?.text, let numbers = Int(numbers_text), let input = Int(input_text) {
+            print("Comparing: \(input_text) minus \(numbers_text) == \(input - numbers)")
+            
+            if(input - numbers == 1111) {
+                print("Correct")
+                MusicHelper.sharedHelper.playCorrectSFX()
+                inputField?.text = ""
+                score += 1
+            }
+            else {
+                print("Incorect")
+                MusicHelper.sharedHelper.playIncorrectSFX()
+                inputField?.text = ""
+                score -= 1
+            }
+        }
+        
+        setRandomNumberLabel()
+        updateScoreLabel()
+        
+    }
+    
+    @objc func onUpdateTimer() -> Void {
+        if(seconds > 0 && seconds <= 60) {
+            seconds -= 1
+            updateTimeLabel()
+        }
+        else if (seconds == 0) {
+            if (timer != nil) {
+                timer!.invalidate()
+                timer = nil
+                
+                saveHighScore(number: score)
+                
+                var alertController = UIAlertController(title: "Time is Up!", message: "Your time is up! Your score is : \(score) points", preferredStyle: .alert)
+                
+                let restartAction = UIAlertAction(title: "Restart", style: .default) {(action) in
+                    
+                    self.score = 0
+                    self.seconds = 60
+                    
+                    self.setTimer()
+                    self.updateTimeLabel()
+                    self.updateScoreLabel()
+                    self.setRandomNumberLabel()
+                }
+                
+                alertController.addAction(restartAction)
+                self.present(alertController, animated: true, completion: nil)
+                
+            }
+        }
+    }
+    
+    func updateTimeLabel() {
+        if(timeLabel != nil) {
+            let min:Int = (seconds / 60) % 60
+            let sec:Int = seconds % 60
+            
+            let min_p:String = String(format: "%02d", min)
+            
+            let sec_p:String = String(format: "%02d",sec)
+            
+            timeLabel!.text = "\(min_p):\(sec_p)"
+        }
+    }
+    
+    func updateScoreLabel() {
+        scoreLabel?.text = "\(score)"
+    }
+    
+    func setRandomNumberLabel() {
+        numbersLabel?.text = generateRandomString()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    func generateRandomString() -> String {
+        var result: String = ""
+        
+        for _ in 1...4 {
+            var digit:Int = Int(arc4random_uniform(8) + 1)
+            
+            result += "\(digit)"
+        }
+        
+        return result
+    }
+    
+    func saveHighScore(number: Int) {
+        if GKLocalPlayer.local.isAuthenticated {
+            let scoreReporter = GKScore(leaderboardIdentifier:  "OBS")
+            scoreReporter.value = Int64(number)
+            
+            let scoreArray: [GKScore] = [scoreReporter]
+            GKScore.report(scoreArray, withCompletionHandler: nil)
+        }
+    }
+    
+//    func playBackgrounMusic(filename: String) {
+//        let url = Bundle.main.url(forResource: filename, withExtension: "mp3")
+//
+//        guard let newURL = url else {
+//            print("Cannot find file: \(filename)")
+//            return
+//        }
+//        do {
+//            backgroundMusicPlayer = try AVAudioPlayer(contentsOf: newURL)
+//            backgroundMusicPlayer.numberOfLoops = -1
+//            backgroundMusicPlayer.prepareToPlay()
+//            backgroundMusicPlayer.play()
+//        } catch let error as NSError {
+//            print(error.description)
+//        }
+//    }
+}
+
+class MusicHelper {
+    static let sharedHelper = MusicHelper()
+    var audioPlayer: AVAudioPlayer?
+    var sfxCorrectPlayer: AVAudioPlayer?
+    var sfxIncorrectPlayer: AVAudioPlayer?
+    
+    func playBGM() {
+
+        let bgm = NSURL(fileURLWithPath: Bundle.main.path(forResource: "Amusing_Day", ofType: "mp3")!)
+
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf:  bgm as URL)
+            audioPlayer!.numberOfLoops = -1
+            audioPlayer!.prepareToPlay()
+            audioPlayer!.play()
+        } catch {
+            print("Cannot Play File")
+        }
+    }
+
+    func stopBGM() {
+        audioPlayer!.stop()
+    }
+
+    func playSong(notification: NSNotification) {
+        audioPlayer?.pause()
+    }
+    
+    func playCorrectSFX() {
+        let sfxCorrect = NSURL(fileURLWithPath: Bundle.main.path(forResource: "correct", ofType: "mp3")!)
+        
+        do {
+            sfxCorrectPlayer = try AVAudioPlayer(contentsOf: sfxCorrect as URL)
+            sfxCorrectPlayer!.prepareToPlay()
+            sfxCorrectPlayer!.play()
+        } catch {
+            print("Can't play sfx correct")
+        }
+    }
+    
+    func playIncorrectSFX() {
+        let sfxIncorrect = NSURL(fileURLWithPath: Bundle.main.path(forResource: "incorrect", ofType: "mp3")!)
+        
+        do {
+            sfxIncorrectPlayer = try AVAudioPlayer(contentsOf: sfxIncorrect as URL)
+            sfxIncorrectPlayer!.prepareToPlay()
+            sfxIncorrectPlayer!.play()
+        } catch {
+            print("Can't play sfx correct")
+        }
+    }
+
+}
---
+++ ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  Onely
+//
+//  Created by Jevier Izza Maulana on 28/04/22.
+//
+
+import UIKit
+import GameKit
+
+class ViewController: UIViewController, GKGameCenterControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        MusicHelper.sharedHelper.playBGM()
+        
+        authPlayer()
+    }
+    
+    
+
+    @IBAction func openGC(_ sender: Any) {
+        let VC = self.view.window?.rootViewController
+        
+        let CGVC = GKGameCenterViewController()
+        
+        CGVC.gameCenterDelegate = self
+        
+        VC?.present(CGVC, animated: false, completion: nil)
+    }
+    
+    func gameCenterViewControllerDidFinish(_ gameCenterViewController: GKGameCenterViewController) {
+        gameCenterViewController.dismiss(animated: false, completion: nil)
+    }
+    
+    
+    func authPlayer() {
+        let localPlayer = GKLocalPlayer.local
+        
+        localPlayer.authenticateHandler = {
+            (view, error) in
+            
+            if view != nil {
+                self.present(view!, animated: true, completion: nil)
+            } else {
+                print(GKLocalPlayer.local.isAuthenticated)
+            }
+        }
+    }
+}
+
